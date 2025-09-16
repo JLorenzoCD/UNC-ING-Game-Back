b685ac28c343f3362a4b46278423de9bb87aee24
hernanmoisset <hernanmoissetm@gmail.com>
2025-09-16 12:26:37 -0300

    ING-46 refactor: specific and general broadcast and newDataClass

--- src/ws_routes.py
+++ src/ws_routes.py
@@ -1,24 +1,53 @@
 from fastapi import WebSocket,FastAPI,WebSocketDisconnect,APIRouter
+import uuid
+from typing import List, Dict, Any, Optional
+from dataclasses import dataclass
 
 websocket_router = APIRouter()
 
+@dataclass
+class ConnectionInfo:
+    ws: WebSocket
+    matchID: Optional[uuid.UUID] = None
+
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: list[ConnectionInfo] = []
 
     async def connect(self, ws: WebSocket):
         await ws.accept()
-        self.active_connections.append(ws)
+        connection=ConnectionInfo(ws=ws, matchID=None)
+        self.active_connections.append(connection)
 
     def disconnect(self, ws: WebSocket):
-        self.active_connections.remove(ws)
+        for connection in self.active_connections:
+            if connection.ws == ws:
+                self.active_connections.remove(connection)
+                break
+    
+    def enterMatch(self, ws:WebSocket, matchID:uuid.UUID):
+        for connection in self.active_connections:
+            if connection.ws == ws:
+                connection.matchID = matchID
+                break
+
+    def quitMatch(self, ws:WebSocket):
+        for connection in self.active_connections:
+            if connection.ws == ws:
+                connection.matchID = None
+                break
 
     async def send_message(self, message: str, ws: WebSocket):
         await ws.send_text(message)
 
-    async def broadcast(self,message:str):
-        for ws in self.active_connections:
-            await ws.send_text(message)
+    async def generalBroadcast(self,message:str):
+        for connection in self.active_connections:
+            await self.send_message(message, connection.ws)
+
+    async def specificBroadcast(self, message:str, matchID: uuid.UUID):
+        for connection in self.active_connections:
+            if connection.matchID == matchID:
+                await self.send_message(message, connection.ws)
             
 
 manager = ConnectionManager()
