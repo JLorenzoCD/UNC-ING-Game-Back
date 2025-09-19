d3894746541324e6a7b35aad1c1a2adde6598607
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-09-19 09:58:41 -0300

    ING-45 Creamos modelo Secret

--- src/app/cards/utils.py
+++
@@ -1,9 +0,0 @@
-from app.cards.models import Card, Match_Card
-from app.cards.schemas import Card_Schema, Match_Card_Schema
-
-def db_card_2_card_schema(db_card: Card) -> Card_Schema:
-    return Card_Schema.model_validate(db_card)
-
-def db_match_card_2_match_card_schema(db_card_match: Match_Card) -> Match_Card_Schema:
-    return Match_Card_Schema.model_validate(db_card_match)
-
