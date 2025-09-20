d3386f4d53312c28a7d6e07caa44c58a64edd010
Elian Ghisolfi <elian.ghisolfi@mi.unc.edu.ar>
2025-09-19 23:13:15 -0300

    ING-8 completamos schemas

--- src/app/matches/schemas.py
+++ src/app/matches/schemas.py
@@ -1,5 +1,7 @@
 from uuid import UUID
+from matches.models import MatchStatus
 from pydantic import BaseModel, ConfigDict
+from matches.dto import MatchDTO
 
 class MatchIn(BaseModel):
     name: str
@@ -7,16 +9,24 @@ class MatchIn(BaseModel):
     max_player: int
     owner_id: UUID
     
-    
+    def to_dto(self) -> MatchDTO:
+        return MatchDTO(
+            name = self.name,
+            min_player = self.min_player,
+            max_player = self.max_player,
+            owner_id = self.owner_id
+        )
+
 class MatchOut(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     id: UUID
     name: str
-    status: enumerate
+    status: MatchStatus
     min_player: int
     max_player: int
     owner_id: UUID
     current_player_order: int
     
-    
+class MatchResponse(MatchOut):
+    pass
