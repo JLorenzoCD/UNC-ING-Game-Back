68918677c37da2343ab1760b6074bde5decb11af
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-09-23 11:49:44 -0300

    ING-22 Creamos archivo de services de match

---
+++ src/app/matches/services.py
@@ -0,0 +1,17 @@
+from app.matches.models import Match, Match_Player
+
+class MatchService:
+    def __init__(self):
+        pass
+
+    def create_match() -> Match:
+        pass
+
+    def get_all_matches() -> list[Match]:
+        pass
+
+    def get_match_by_id() -> Match | None:
+        pass
+
+    def update_match() -> Match:
+        pass
