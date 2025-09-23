2fe4e6d27b0e11036e6efbdb1c16c633fb869859
hernanmoisset <hernanmoissetm@gmail.com>
2025-09-23 12:10:07 -0300

    ING-9 endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from websocketManager.ws_routes import websocket_router
 from app.models.db import Base, engine
+from app.player.endpoints import player_router
 
 app = FastAPI()
 app.include_router(websocket_router)
+app.include_router(player_router)
 Base.metadata.create_all(bind=engine)
 # Aca tendriamos que llamar a una funcion que inicialice todos los datos (cartas,secretos,etc)
