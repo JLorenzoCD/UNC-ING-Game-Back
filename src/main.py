df1426444281b6113049c095f649ec12a26a25ad
hernanmoisset <hernanmoissetm@gmail.com>
2025-09-23 12:11:34 -0300

    ING-9 problema CORS

--- src/main.py
+++ src/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from websocketManager.ws_routes import websocket_router
 from app.models.db import Base, engine
 from app.player.endpoints import player_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # o ["*"] para todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(websocket_router)
 app.include_router(player_router)
 Base.metadata.create_all(bind=engine)
