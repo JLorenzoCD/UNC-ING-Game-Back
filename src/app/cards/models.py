f775d06c3954bf6b0cd87a8f5cccfebc312b99e3
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-09-23 17:11:32 -0300

    ING-54 Añadimos el atributo type

--- src/app/cards/models.py
+++ src/app/cards/models.py
@@ -2,8 +2,17 @@ from sqlalchemy import Column, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
+from enum import Enum as PyEnum
+from sqlalchemy import Enum as SQlEnum, Enum
+
 
 from app.models.db import Base
+
+class Card_Type(PyEnum):
+    EVENT     = "EVENT"
+    DEVIUS    = "DEVIUS"
+    DETECTIVE = "DETECTIVE"
+    INSTANT   = "INSTANT"
 
 class Card(Base):
 
@@ -11,6 +20,7 @@ class Card(Base):
 
     id          = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name        = Column(String, index=True, nullable=False)
+    type        = Column(Enum(Card_Type), index=True, nullable=False)
     description = Column(String, nullable=False)
 
 class Match_Card(Base):
--- src/app/cards/tests/test_card.py
+++ src/app/cards/tests/test_card.py
@@ -1,6 +1,6 @@
 import pytest
 from uuid import uuid4
-from app.cards.models import Card
+from app.cards.models import Card, Card_Type    
 from app.cards.schemas import Card_Schema
 from app.cards.utils import db_card_2_card_schema
 
@@ -8,7 +8,7 @@ print("Test module loaded successfully!")  # Debugfrom ..utils import *
 
 def test_create_card(db):
     # Creamos una Card en la DB
-    new_card = Card(name="Espada mágica", description="Espada +10")
+    new_card = Card(name="Espada mágica", type=Card_Type.EVENT, description="Espada +10")
     db.add(new_card)
     db.commit()
     db.refresh(new_card)
@@ -16,6 +16,7 @@ def test_create_card(db):
     # Verificamos que se guardó
     assert new_card.id is not None
     assert new_card.name == "Espada mágica"
+    assert new_card.type == Card_Type.EVENT
     assert new_card.description == "Espada +10"
 
     # Convertimos con el schema
@@ -25,10 +26,12 @@ def test_create_card(db):
     assert isinstance(schema_card, Card_Schema)
     assert schema_card.id == new_card.id
     assert schema_card.name == new_card.name
+    assert schema_card.type == new_card.type
     assert schema_card.description == new_card.description
 
     # Verificamos que podemos serializar a dict
     card_dict = schema_card.model_dump()
     assert "id" in card_dict
     assert card_dict["name"] == "Espada mágica"
+    assert card_dict["type"] == Card_Type.EVENT
     assert card_dict["description"] == "Espada +10"
