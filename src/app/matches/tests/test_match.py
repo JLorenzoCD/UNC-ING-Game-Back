7f91ece2f871eb8397f1aeb6438e29a74768dfa0
Elian Ghisolfi <elian.ghisolfi@mi.unc.edu.ar>
2025-09-25 10:09:32 -0300

    ING-10 Bug-Fix de atributo en Match -> min_players and max_players

--- src/app/matches/tests/test_match.py
+++ src/app/matches/tests/test_match.py
@@ -32,8 +32,8 @@ def test_create_match(db_session):
     
     new_match = Match(
         name = "AGE 3",
-        min_player = 4,
-        max_player = 6,
+        min_players = 4,
+        max_players = 6,
         owner_id = new_player_owner.id
         )
     
@@ -44,8 +44,8 @@ def test_create_match(db_session):
     #Testeo de entidad Match
     
     assert new_match.name == "AGE 3"
-    assert new_match.min_player == 4
-    assert new_match.max_player == 6
+    assert new_match.min_players == 4
+    assert new_match.max_players == 6
     assert new_match.owner_id is not None
     assert new_match.owner_id == new_player_owner.id
     assert new_match.status == MatchStatus.WAITING
@@ -61,8 +61,8 @@ def test_create_match(db_session):
     assert new_schemas_match.id == new_match.id
     assert new_schemas_match.name == new_match.name
     assert new_schemas_match.status == new_match.status
-    assert new_schemas_match.min_player == new_match.min_player
-    assert new_schemas_match.max_player == new_match.max_player
+    assert new_schemas_match.min_players == new_match.min_players
+    assert new_schemas_match.max_players == new_match.max_players
     assert new_schemas_match.owner_id == new_match.owner_id
     assert new_schemas_match.current_player_order == new_match.current_player_order
     
@@ -73,7 +73,7 @@ def test_create_match(db_session):
     assert "id" in match_dict
     for key in [
         "id", "name", "status",
-        "min_player", "max_player",
+        "min_players", "max_players",
         "owner_id", "current_player_order"
     ]:
         assert key in match_dict
@@ -101,8 +101,8 @@ def test_create_match(db_session):
     dto = match_in.to_dto()
     assert isinstance(dto, MatchDTO)
     assert dto.name == match_in.name
-    assert dto.min_player == match_in.min_players
-    assert dto.max_player == match_in.max_players
+    assert dto.min_players == match_in.min_players
+    assert dto.max_players == match_in.max_players
     assert dto.owner_id == match_in.owner_id
 
     # Serializar el schema a dict
