a035dc99d3ddb290a2bbcd9c703448b45e305e76
Elian Ghisolfi <elian.ghisolfi@mi.unc.edu.ar>
2025-09-25 01:14:36 -0300

    ING-10 correcion de rutas para los endpoint de matches

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from app.secrets.models import Secret, Match_Secret
 
 from app.models.db import Base, engine
 from app.player.endpoints import player_router
+from app.matches.endpoints import router as matches_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
