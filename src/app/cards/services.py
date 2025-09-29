cc3394dbbe515fa986ab7c74d285ead077f013e7
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-09-29 02:13:34 -0300

    ING-22 Arreglamos bugs con la reparticion de secretos y la generacion de cartas

--- src/app/cards/services.py
+++ src/app/cards/services.py
@@ -7,31 +7,54 @@ class Cards_Services:
     def __init__(self, db):
         self._db = db
 
-    def init_match_cards(self, match_id: UUID) -> None:
+    def init_match_cards(self, match_id: UUID, num_players: int) -> None:
         # Lista de cartas con cantidad
-        all_cards = [
-            {"type": "INSTANT", "name": "NOT SO FAST", "quantity": 10},
-            {"type": "DETECTIVE", "name": "PARKER PYNE", "quantity": 3},
-            {"type": "DETECTIVE", "name": "LADY EILEEN", "quantity": 3},
-            {"type": "DETECTIVE", "name": "TOMMY BERESFORD", "quantity": 2},
-            {"type": "DETECTIVE", "name": "TUPPENCE BERESFORD", "quantity": 2},
-            {"type": "DETECTIVE", "name": "HARLEY QUIN WILDCARD", "quantity": 4},
-            {"type": "DETECTIVE", "name": "ARIADNE OLIVER", "quantity": 3},
-            {"type": "DETECTIVE", "name": "HERCULE POIROT", "quantity": 1},
-            {"type": "DETECTIVE", "name": "MISS MARPLE", "quantity": 3},
-            {"type": "DETECTIVE", "name": "MR SATTERTHWAITE", "quantity": 2},
-            {"type": "EVENT", "name": "CARDS OFF THE TABLE", "quantity": 1},
-            {"type": "EVENT", "name": "ANOTHER VICTIM", "quantity": 2},
-            {"type": "EVENT", "name": "DEAD CARD FOLLY", "quantity": 3},
-            {"type": "EVENT", "name": "LOOK INTO THE ASHES", "quantity": 3},
-            {"type": "EVENT", "name": "CARD TRADE", "quantity": 2},
-            {"type": "EVENT", "name": "AND THEN THERE WAS ONE MORE", "quantity": 2},
-            {"type": "EVENT", "name": "DELAY THE MURDERER ESCAPE", "quantity": 3},
-            {"type": "EVENT", "name": "EARLY TRAIN TO PADDINGTON", "quantity": 2},
-            {"type": "EVENT", "name": "POINT YOUR SUSPICIONS", "quantity": 3},
-            {"type": "DEVIOUS", "name": "BLACKMAILED", "quantity": 3},
-            {"type": "DEVIOUS", "name": "SOCIAL FAUX PAS", "quantity": 3},
-        ]
+        if num_players == 2:
+            all_cards = [
+                {"type": "INSTANT", "name": "NOT SO FAST", "quantity": 10},
+                {"type": "DETECTIVE", "name": "PARKER PYNE", "quantity": 3},
+                {"type": "DETECTIVE", "name": "LADY EILEEN", "quantity": 3},
+                {"type": "DETECTIVE", "name": "TOMMY BERESFORD", "quantity": 2},
+                {"type": "DETECTIVE", "name": "TUPPENCE BERESFORD", "quantity": 2},
+                {"type": "DETECTIVE", "name": "HARLEY QUIN WILDCARD", "quantity": 4},
+                {"type": "DETECTIVE", "name": "ARIADNE OLIVER", "quantity": 3},
+                {"type": "DETECTIVE", "name": "HERCULE POIROT", "quantity": 1},
+                {"type": "DETECTIVE", "name": "MISS MARPLE", "quantity": 3},
+                {"type": "DETECTIVE", "name": "MR SATTERTHWAITE", "quantity": 2},
+                {"type": "EVENT", "name": "CARDS OFF THE TABLE", "quantity": 1},
+                {"type": "EVENT", "name": "ANOTHER VICTIM", "quantity": 2},
+                {"type": "EVENT", "name": "DEAD CARD FOLLY", "quantity": 3},
+                {"type": "EVENT", "name": "LOOK INTO THE ASHES", "quantity": 3},
+                {"type": "EVENT", "name": "CARD TRADE", "quantity": 2},
+                {"type": "EVENT", "name": "AND THEN THERE WAS ONE MORE", "quantity": 2},
+                {"type": "EVENT", "name": "DELAY THE MURDERER ESCAPE", "quantity": 3},
+                {"type": "EVENT", "name": "EARLY TRAIN TO PADDINGTON", "quantity": 2},
+                {"type": "DEVIOUS", "name": "SOCIAL FAUX PAS", "quantity": 3},
+            ]
+        else:
+            all_cards = [
+                {"type": "INSTANT", "name": "NOT SO FAST", "quantity": 10},
+                {"type": "DETECTIVE", "name": "PARKER PYNE", "quantity": 3},
+                {"type": "DETECTIVE", "name": "LADY EILEEN", "quantity": 3},
+                {"type": "DETECTIVE", "name": "TOMMY BERESFORD", "quantity": 2},
+                {"type": "DETECTIVE", "name": "TUPPENCE BERESFORD", "quantity": 2},
+                {"type": "DETECTIVE", "name": "HARLEY QUIN WILDCARD", "quantity": 4},
+                {"type": "DETECTIVE", "name": "ARIADNE OLIVER", "quantity": 3},
+                {"type": "DETECTIVE", "name": "HERCULE POIROT", "quantity": 1},
+                {"type": "DETECTIVE", "name": "MISS MARPLE", "quantity": 3},
+                {"type": "DETECTIVE", "name": "MR SATTERTHWAITE", "quantity": 2},
+                {"type": "EVENT", "name": "CARDS OFF THE TABLE", "quantity": 1},
+                {"type": "EVENT", "name": "ANOTHER VICTIM", "quantity": 2},
+                {"type": "EVENT", "name": "DEAD CARD FOLLY", "quantity": 3},
+                {"type": "EVENT", "name": "LOOK INTO THE ASHES", "quantity": 3},
+                {"type": "EVENT", "name": "CARD TRADE", "quantity": 2},
+                {"type": "EVENT", "name": "AND THEN THERE WAS ONE MORE", "quantity": 2},
+                {"type": "EVENT", "name": "DELAY THE MURDERER ESCAPE", "quantity": 3},
+                {"type": "EVENT", "name": "EARLY TRAIN TO PADDINGTON", "quantity": 2},
+                {"type": "EVENT", "name": "POINT YOUR SUSPICIONS", "quantity": 3},
+                {"type": "DEVIOUS", "name": "BLACKMAILED", "quantity": 3},
+                {"type": "DEVIOUS", "name": "SOCIAL FAUX PAS", "quantity": 3},
+            ]
 
         # Crear Match_Card según la cantidad
         for card_info in all_cards:
