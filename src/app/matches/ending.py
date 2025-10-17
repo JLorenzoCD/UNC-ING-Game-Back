d400cc224f085944f6a45cb4476f1d73c12c2da0
hernanmoisset <hernanmoissetm@gmail.com>
2025-10-16 23:54:56 -0300

    ING-35 creacion del payload correspondiente y tests de ending

--- src/app/matches/ending.py
+++ src/app/matches/ending.py
@@ -9,37 +9,49 @@ class MatchEndedReason(Enum):
     MURDERER_REVEALED = "murderer_revealed"
 
 async def handle_match_ended(db, manager, match_id: UUID,
-                             reason: MatchEndedReason,
-                             murderer_name: str,
-                             accomplice_name: Optional[str]):
+                             reason: MatchEndedReason
+                             ):
     #imports locales para no destruir todo con dependencia circulares
     from app.matches.services import MatchService
+    from app.secrets.services import Secrets_Services
     from websocketManager.ws_messages import WSEvent, make_ws_message
 
     try:
         MatchService(db).update_status_match(match_id, MatchStatus.COMPLETED)
     except SQLAlchemyError:
         pass
-    if accomplice_name:
-        payload = {
-            "match_id": str(match_id),
-            "reason": str(reason.value),
-            "details": f"The murderer was {murderer_name} and the accomplice was {accomplice_name}"
-        }
+    
+
+    info=Secrets_Services(db).get_full_info(match_id)
+    detailstmp=""
+    if reason==MatchEndedReason.MURDERER_REVEALED:
+        if info['accomplice_name']:
+            detailstmp=f"El asesino fue revelado. {info['murderer_name']} era el asesino y {info['accomplice_name']} era su cómplice"
+        else:
+            detailstmp=f"El asesino fue revelado. {info['murderer_name']} era el asesino"
     else:
-        payload = {
-            "match_id": str(match_id),
-            "reason": str(reason.value),
-            "details": f"The murderer was {murderer_name}"
-        }
+        if info['accomplice_name']:
+            detailstmp=f"El asesino {info['murderer_name']} se escapo!. Fue ayudado por su cómplice {info['accomplice_name']}"
+        else:
+            detailstmp=f"El asesino {info['murderer_name']} se escapo!."
+            
+            
+    payload = {
+        "match_id": str(match_id),
+        "secret_murderer_id": str(info['murderer_secret_id']) ,
+        "secret_accomplice_id": str(info['accomplice_secret_id']) if info['accomplice_secret_id'] else None,
+        "reason": reason.value,
+        "details": detailstmp
+    }
+    
     try:
         await manager.specificBroadcast(make_ws_message(WSEvent.MATCH_ENDED, payload), match_id)
-    except Exception:
-        pass
+    except Exception as e:
+        print(f"Error al enviar WS: {e}")
 
     try:
          manager.close_match(match_id)
-    except Exception:
-        pass
+    except Exception as e:
+        print(f"Error al cerrar la partida: {e}")
     
     return payload
