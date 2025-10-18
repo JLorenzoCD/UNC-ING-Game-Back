a9e9431169d2021214264cd67c63214c47f9e7ad
hernanmoisset <hernanmoissetm@gmail.com>
2025-10-18 13:40:59 -0300

    ING-77 update delay_the_murderer_escape

--- src/app/matches/endpoints.py
+++ src/app/matches/endpoints.py
@@ -421,10 +421,12 @@ async def play_event(match_id:UUID, player_id: UUID, match_card_id:UUID, event_p
                 raise ValueError("Se pasaron mas de 5 cartas para retrasar")
             if len(event_payload.card_ids) == 0:
                 #se pasaron 0 cartas podria pasar si es la primera carta que se juega y no hay nada en la pila de descarte
-                print("a")
-            services_cards.Cards_Services(db).look_into_the_ashes_event(event_payload.cards_uds)
-
-            #hace algo
+                print("Debe poderse jugar")
+            updated_match_cards=services_cards.Cards_Services(db).look_into_the_ashes_event(event_payload.cards_uds)
+            updated_card_event=services_cards.Cards_Services(db).discard_card(match_card_id)
+            
+            #hacer el payload para la devolucion por ws
+            
             return 0
         case Card_event.EARLY_TRAIN_TO_PADDINGTON:
             #hace algo
