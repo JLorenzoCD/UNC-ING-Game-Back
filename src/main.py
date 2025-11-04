66fa079524865cd0986d09c7480c320feec4f69b
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-11-04 12:27:32 -0300

    ING-141 Arreglamos cantidad de cartas

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-import uuid
 from sqlalchemy.orm import Session
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.models.db import Base, engine
-from app.cards.models import Card, Card_Type
-from app.secrets.models import Secret, Match_Secret, Secret_Type
+from app.database.init_service import init_data
 from app.player.endpoints import player_router
 from app.matches.endpoints import router as matches_router
 from websocketManager.ws_routes import websocket_router
@@ -14,17 +12,11 @@ from websocketManager.ws_routes import websocket_router
 app = FastAPI()
 
 app.add_middleware(
-
     CORSMiddleware,
-
     allow_origins=["http://localhost:5173"],  # o ["*"] para todos los orígenes
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
-
 )
 
 
@@ -32,49 +24,8 @@ app.include_router(websocket_router)
 app.include_router(player_router)
 app.include_router(matches_router)
 
-def init_data():
-    with Session(engine) as session:
-        # Verificar si ya existen secretos
-        if not session.query(Secret).first():
-            session.add_all([
-                Secret(id=uuid.uuid4(), type="MURDERER", content="You are the Murderer!"),
-                Secret(id=uuid.uuid4(), type="ACCOMPLICE", content="You are the Accomplice!"),
-                Secret(id=uuid.uuid4(), type="INNOCENT", content="You are Innocent!")
-            ])
-            session.commit()
-        
-        if not session.query(Card).first():
-            session.add_all([
-                # Instant Cards
-                Card(id=uuid.uuid4(), name="NOT SO FAST", type=Card_Type.INSTANT, description="Play this card at any time, even if it is not your turn. It cancels an action before it is taken, unless otherwise stated, including cancelling another 'Not so fast...' card."),
-        
-                # Detective Cards
-                Card(id=uuid.uuid4(), name="PARKER PYNE", type=Card_Type.DETECTIVE, description="Parker Pyne focuses on helping his clients achieve happiness... Instead of revealing a secret card, flip any face-up secret card face-down. This may remove social disgrace."),
-                Card(id=uuid.uuid4(), name="LADY EILEEN", type=Card_Type.DETECTIVE, description="Bundle realises a vital clue was missed... Choose a player who must reveal a secret card of their choice. If cancelled by a 'Not so fast...' card, return the detective set to your hand."),
-                Card(id=uuid.uuid4(), name="TOMMY BERESFORD", type=Card_Type.DETECTIVE, description="Tommy finds a clue... Choose a player, who must reveal a secret card of their choice. If a Tommy and a Tuppence are in the same set, the action cannot be cancelled by a 'Not so Fast...' card."),
-                Card(id=uuid.uuid4(), name="TUPPENCE BERESFORD", type=Card_Type.DETECTIVE, description="Tuppence finds a clue... Choose a player, who must reveal a secret card of their choice. If a Tuppence and Tommy are in the same set, the action cannot be cancelled by a 'Not so Fast...' card."),
-                Card(id=uuid.uuid4(), name="HARLEY QUIN WILDCARD", type=Card_Type.DETECTIVE, description="Quin has an almost supernatural gift for helping solve the crime... Play in conjunction with any original detective card to play a set in front of you."),
-                Card(id=uuid.uuid4(), name="ARIADNE OLIVER", type=Card_Type.DETECTIVE, description="Mrs Oliver spots something that others had missed... Add to any existing set on the table. The player owning the set must reveal a secret card of their choice. May only be added, and not played as a set."),
-                Card(id=uuid.uuid4(), name="HERCULE POIROT", type=Card_Type.DETECTIVE, description="Poirot finds a suspicious document once thought lost... Choose a player, who must reveal a secret card of your choice."),
-                Card(id=uuid.uuid4(), name="MISS MARPLE", type=Card_Type.DETECTIVE, description="Miss Marple notices a small detail that grows in importance... Choose a player, who must reveal a secret card of your choice."),
-                Card(id=uuid.uuid4(), name="MR SATTERTHWAITE", type=Card_Type.DETECTIVE, description="Mr Satterthwaite finds an odd thing... Choose a player, who must reveal a secret card of their choice. If this set is played with a Harley Quin Wildcard, add the revealed secret card, face-down, to your secrets."),
-                
-                # Event Cards
-                Card(id=uuid.uuid4(), name="CARDS OFF THE TABLE", type=Card_Type.EVENT, description="Their plans are discovered! Choose a player, who must discard all the 'Not so fast...' cards in their hand. The action cannot be cancelled by a 'Not so fast...' card."),
-                Card(id=uuid.uuid4(), name="ANOTHER VICTIM", type=Card_Type.EVENT, description="Take any existing set from another player and play it in front of you. You now own this set."),
-                Card(id=uuid.uuid4(), name="DEAD CARD FOLLY", type=Card_Type.EVENT, description="All players must pass one card from their hand, face-down, to the player on their right or left. The active player decides which direction. You may ask for a card of your choice, but beware you may be tricked."),
-                Card(id=uuid.uuid4(), name="LOOK INTO THE ASHES", type=Card_Type.EVENT, description="A pattern emerges! You may look though the top five cards of the discard pile and take one into your hand."),
-                Card(id=uuid.uuid4(), name="CARD TRADE", type=Card_Type.EVENT, description="Meet in the drawing room for high tea. Choose another player and exchange one card from your hand with them. They cannot refuse. You may ask for a card of your choice, but beware you may be tricked."),
-                Card(id=uuid.uuid4(), name="AND THEN THERE WAS ONE MORE", type=Card_Type.EVENT, description="Choose one revealed secret card and add it, face-down, to any player's secrets, including your own. This may remove social disgrace."),
-                Card(id=uuid.uuid4(), name="DELAY THE MURDERER ESCAPE", type=Card_Type.EVENT, description="Take up to five cards from the top of the discard pile and place them face-down on the draw pile in any order, then remove this card from the game."),
-                Card(id=uuid.uuid4(), name="EARLY TRAIN TO PADDINGTON", type=Card_Type.EVENT, description="The murderer's escape nears! Take the top six cards from the draw pile and place them face-up on the discard pile, then remove this card from the game. Discarding this card is treated the same as if you had played it."),
-                Card(id=uuid.uuid4(), name="POINT YOUR SUSPICIONS", type=Card_Type.EVENT, description="All the guests are gathered in the dining room... The active player counts down: 3-2-1. Then all players must point at the person they suspect as the Murderer. The active player breaks ties. The most suspected player must reveal a secret card of their choice."),
-                
-                # Devious Cards
-                Card(id=uuid.uuid4(), name="BLACKMAILED", type=Card_Type.DEVIOUS, description="You have been blackmailed! If you have received this card from another player, you must show them one secret card of their choice, before returning it face-down to your secrets. This action cannot be cancelled by a 'Not so fast...' card. This card can only be used during a Card Trade or a Dead Card Folly."),
-                Card(id=uuid.uuid4(), name="SOCIAL FAUX PAS", type=Card_Type.DEVIOUS, description="At dinner, you have been tricked into ordering a dessert wine with a starter... If you have received this card from another player, you must reveal a secret card of your choice. This card can only be used during a Card Trade or a Dead Card Folly.")
-            ])
-            session.commit()
-
 Base.metadata.create_all(bind=engine)
-init_data()
+
+# Inicializar datos base
+with Session(engine) as session:
+    init_data(session)
