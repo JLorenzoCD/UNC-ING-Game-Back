ae4822340f575ad8fe69d955fd64fe51ec70a0c8
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-11-06 08:55:12 -0300

    ING-162 Añadimos servicio para buscar log por id

--- src/app/matches/services.py
+++ src/app/matches/services.py
@@ -520,7 +520,7 @@ class LogService:
     def __init__(self, db):
         self._db = db
 
-    def create_log(self, match_id: UUID, message: str, event_type: str, player_id: Optional[UUID] = None) -> None:
+    def create_log(self, match_id: UUID, message: str, event_type: str, player_id: Optional[UUID] = None) -> UUID:
         new_log = MatchLogs(
             match_id   = match_id,
             message    = message,
@@ -532,6 +532,7 @@ class LogService:
             self._db.add(new_log)
             self._db.commit()
             self._db.refresh(new_log)
+            return new_log.id
         except SQLAlchemyError as exception:
             self._db.rollback()
             raise exception
@@ -540,3 +541,11 @@ class LogService:
         result = self._db.query(MatchLogs).filter(MatchLogs.match_id == match_id).all()
 
         return [db_match_log_2_match_log_schema(match_log) for match_log in result]
+    
+    def get_log_by_id(self, log_id: UUID) -> match_schemas.MatchLogOut:
+        try:
+            log = self._db.query(MatchLogs).filter(log_id).first()
+            return db_match_log_2_match_log_schema(log)
+        except SQLAlchemyError as exception:
+            self._db.rollback()
+            raise exception
