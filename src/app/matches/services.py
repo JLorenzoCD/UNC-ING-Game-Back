26b4dae393d14bc27b17d12a23d2ad8dee167455
Nicolás <nicolas.jorge@mi.unc.edu.ar>
2025-11-10 16:39:06 -0300

    ING-154 Funciona el mantener estado de la partida

--- src/app/matches/services.py
+++ src/app/matches/services.py
@@ -124,6 +124,25 @@ class MatchService:
             raise
         return match
 
+    def get_ongoing_matches_of_player(self, player_id: UUID) -> List[UUID]:
+        try:
+            match_players = (
+                self._db.query(Match_Player)
+                .join(Match, Match_Player.match_id == Match.id)
+                .filter(
+                    Match_Player.player_id == player_id,
+                    Match.status != MatchStatus.COMPLETED,
+                )
+                .all()
+            )
+            
+            return [mp.match_id for mp in match_players]
+        except SQLAlchemyError:
+            self._db.rollback()
+            raise
+        except Exception:
+            raise
+
     def get_active_matches_ids_player(self, player_id: UUID) -> List[UUID]:
         try:
             match_players = (
