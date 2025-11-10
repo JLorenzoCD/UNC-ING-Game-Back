6500ba530b729150a4b2dd2c357d6648340634af
Elian Ghisolfi <elian.ghisolfi@mi.unc.edu.ar>
2025-11-09 22:41:16 -0300

    ING-145 debug condiciones de Oliver

--- src/app/matches/endpoints.py
+++ src/app/matches/endpoints.py
@@ -411,7 +411,7 @@ async def put_down_a_detective(match_id: UUID, set_id:UUID, setIn:set_schemas.Ad
 
         card_name = set_service._get_card_names(match_card_ids)[0]
         
-        if match_set.type != SetType.LADY_EILEEN:
+        if match_set.type != SetType.LADY_EILEEN or card_name == SetType.ADRIADNE_OLIVER.value:
         # Actualizamos si es el caso de que agregamos a TOMMY_BERESFORD en  TUPPENCE_BERESFORD y viceversa
             if set_service.beresford_brothers_in_set_two_beresford(card_name, match_set.type.value):
                 match_set = set_service.update_setType(set_id, SetType.TWO_BERESFORD)
