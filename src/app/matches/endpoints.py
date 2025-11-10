a66123a7134395c337451eec7d768e5944139dcb
Elian Ghisolfi <elian.ghisolfi@mi.unc.edu.ar>
2025-11-10 20:46:19 -0300

    ING-155 Debug Final final

--- src/app/matches/endpoints.py
+++ src/app/matches/endpoints.py
@@ -449,7 +449,7 @@ async def time_out(match_id: UUID, player_id:UUID, db = Depends(get_db)) -> Opti
                         ).order_by(Match_Card.id).offset(3).first()
         
         if fourth_card:
-            new_card = services.PileService(db).take_cards(player_id, match_id, cards=[fourth_card])
+            new_card = services.PileService(db).take_cards(player_id, match_id, cards=[fourth_card.id])
         else:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
